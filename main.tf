450a219822f5cfcf6d016f0c7ea84d21397745e8
Matthew Chenette <mchenette@microsoft.com>
2022-08-12 16:38:43 -0500

    added an app instance

--- main.tf
+++ main.tf
@@ -13,8 +13,19 @@ provider "azuread" {
   tenant_id = "b3cbb45e-6152-433f-bfe2-ac90a78d1408"
 }
 
-resource "azuread_group" "aad_group" {
-  display_name     = "example"
+resource "azuread_group" "aad_aao_group" {
+  display_name     = "Administrative Applications - Owners"
   owners           = ["84574441-38cc-4302-be53-903f57446fdb"]
+  prevent_duplicate_names = true
   security_enabled = true
+  visibility = "Public"
 }
+
+resource "azuread_application" "aad_app" {
+  display_name = "Application Creator"
+  owners = [resource.aad_aao_group.object_id]
+  prevent_duplicate_names = true
+}
+
+
+# because of https://docs.microsoft.com/en-us/graph/permissions-reference#remarks-5, we will be granting this app Application.ReadWrite.All instead of Directory.ReadWrite.All in order to grant it the lowest needed level of permissions to do its job of creating other applications and service principals
