783672fa5a014a8494db3baa287b65f0d43a2ba3
Matthew Chenette <mchenette@microsoft.com>
2022-08-13 13:47:26 -0500

    added subscription_id

--- tfstate.tf
+++ tfstate.tf
@@ -1,9 +1,10 @@
 terraform {}
 
 provider "azurerm" {
-  use_oidc  = true
-  client_id = "6f8856bb-855f-462b-8c4c-4bfe34cc4d84"
-  tenant_id = "b3cbb45e-6152-433f-bfe2-ac90a78d1408"
+  use_oidc        = true
+  client_id       = "6f8856bb-855f-462b-8c4c-4bfe34cc4d84"
+  tenant_id       = "b3cbb45e-6152-433f-bfe2-ac90a78d1408"
+  subscription_id = "169aa2b8-a099-41ff-b9d6-0447c8a3c047"
 
   features {}
 }
