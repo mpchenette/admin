83306da762e2e098b0b255c4af248af50fe38d5e
Matthew Chenette <mchenette@microsoft.com>
2022-08-13 21:52:02 -0500

    added oidc

--- main.tf
+++ main.tf
@@ -13,15 +13,16 @@ terraform {
       version = "~> 3.18.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "rg-terraformstate-shared-001"
+    storage_account_name = "stterraformstate001"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+    use_oidc             = true
+    tenant_id            = "b3cbb45e-6152-433f-bfe2-ac90a78d1408"
+    subscription_id      = "169aa2b8-a099-41ff-b9d6-0447c8a3c047"
 
-    backend "azurerm" {
-        resource_group_name  = "rg-terraformstate-shared-001"
-        storage_account_name = "stterraformstate001"
-        container_name       = "tfstate"
-        key                  = "terraform.tfstate"
-    }
-
-
+  }
   required_version = ">= 1.1.0"
 }
 
